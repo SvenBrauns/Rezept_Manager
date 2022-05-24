1898e67b271063bd5fcd45539b3eb11d1c302afb
SvenBrauns <sven@brauns-bdf.de>
2022-05-24 18:53:27 +0200

    Small Changes

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
-class Todo(db.Model):
+""" class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name= db.Column(db.String(200), nullable=False)
     content = db.Column(db.String(200), nullable=False)
@@ -14,7 +14,26 @@ class Todo(db.Model):
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return '<Task %r>' % self.id
+        return '<Task %r>' % self.id """
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_name= db.Column(db.String(200), nullable=False)
+    cook_time = db.Column(db.String(200), nullable=False)
+    ingredients = db.relationship('Ingredients', backref='recipe', lazy=True)
+
+    """ def __repr__(self):
+        return '<Recipe %r>' % self.id """
+
+class Ingredients(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'))
+    name= db.Column(db.String(200), nullable=False)
+    quantity = db.Column(db.Float, primary_key=False)
+    unit = db.Column(db.String(200), nullable=False)
+
+    """ def __repr__(self) -> str:
+        return '<Ingredients %r>' % self.id """   
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
